ee061041c727b2a7d03af83d9df630abc60e64e0
Prajwa-l-Kumar <prajwalkumar.bt22cse@pec.edu.in>
2023-03-08 23:19:53 +0530

    Updated Micromouse.java

--- main/Micromouse.java
+++ main/Micromouse.java
@@ -22,7 +22,7 @@ public class Micromouse {
  */
     static int size = 5;
     static boolean hwalls[][] = new boolean[size][size];
-    static int hwallsVal[][] = {{0,0}, {1,0}, {2,0}, {3,0}, {4,0}, {1,1}, {2,0}, {0,2}, {3,2}, {2,3}, {3,4}};
+    static int hwallsVal[][] = {{0,0}, {1,0}, {2,0}, {3,0}, {4,0}, {1,1}, {2,0}, {2,1}, {0,2}, {3,2}, {2,3}, {3,4}};
     static boolean vwalls[][] = new boolean[size][size];
     static int vwallsVal[][] = {{0,0}, {0,1}, {0,2}, {0,3}, {0,4}, {1,3}, {1,4}, {2,1}, {2,2}, {2,3}, {3,0}, {3,2}, {3,3}, {4,1}};
     static int dist[][] = new int[size][size];
@@ -31,6 +31,7 @@ public class Micromouse {
     static char dirs[] = {'N', 'E', 'S', 'W'};
     static char dir;
     static boolean goal;
+    static int targetx, targety;
 
     public static void setup() {
         for(int i = 0; i < hwallsVal.length; i++) {
@@ -59,6 +60,9 @@ public class Micromouse {
         posy = size - 1;
 
         goal = false;
+
+        targetx = size / 2;
+        targety = size / 2;
     }
 
     public static void printMatrix() {
@@ -141,42 +145,79 @@ public class Micromouse {
         }
     }
 
+    /*
+     * return the neighbouring neighbour cell whose manhattan dist is less than the current cell if present
+     * else return invalid cell
+     */
+    public static Cell lesserAvailableNeighbourCell(Cell cell) {
+        Cell retCell = new Cell(-1, -1, false);
+        if(cell.x < size - 1) { //E
+            if(dist[cell.y][cell.x + 1] < dist[cell.y][cell.x] && !vwalls[cell.x + 1][cell.y]) {
+                retCell.res = true;
+                retCell.x = cell.x;
+                retCell.y = cell.y;
+                return retCell;
+            }
+        }
+        if(cell.x > 0) { //W
+            if(dist[cell.y][cell.x - 1] < dist[cell.y][cell.x] && !vwalls[cell.x][cell.y]) {
+                retCell.res = true;
+                retCell.x = cell.x;
+                retCell.y = cell.y;
+                return retCell;
+            }
+        }
+        if(cell.y < size - 1) { //S
+            if(dist[cell.y + 1][cell.x] < dist[cell.y][cell.x] && !hwalls[cell.x][cell.y + 1]) {
+                retCell.res = true;
+                retCell.x = cell.x;
+                retCell.y = cell.y;
+                return retCell;
+            }
+        }
+        if(cell.y > 0) { //N
+            if(dist[cell.y - 1][cell.x] < dist[cell.y][cell.x] && !hwalls[cell.x][cell.y]) {
+                retCell.res = true;
+                retCell.x = cell.x;
+                retCell.y = cell.y;
+                return retCell;
+            }
+        }
+        return retCell;
+    }
 
-    public static Cell LessAvailableNeighbour() {
-        Cell cell = new Cell(-1, -1, false);
-        if(posx < size - 1) { //E
-            if(dist[posy][posx + 1] < dist[posy][posx] && !vwalls[posx + 1][posy]) {
-                cell.res = true;
-                cell.x = posx;
-                cell.y = posy;
-                return cell;
+    public static Cell leastAvailableNeighbourCell(Cell cell) {
+        Cell retCell = new Cell(-1, -1, false);
+        int leastDist = size;
+        if(cell.x < size - 1) { //E
+            if(!vwalls[posx + 1][posy] && dist[cell.y][cell.x + 1] < leastDist) {
+                retCell.x = cell.x + 1;
+                retCell.y = cell.y;
+                leastDist = dist[cell.y][cell.x + 1];
             }
         }
-        if(posx > 0) { //W
-            if(dist[posy][posx - 1] < dist[posy][posx] && !vwalls[posx][posy]) {
-                cell.res = true;
-                cell.x = posx;
-                cell.y = posy;
-                return cell;
+        if(cell.x > 0) { //W
+            if(!vwalls[posx][posy] && dist[cell.y][cell.x - 1] < leastDist) {
+                retCell.x = cell.x - 1;
+                retCell.y = cell.y;
+                leastDist = dist[cell.y][cell.x - 1];
             }
         }
-        if(posy < size - 1) { //S
-            if(dist[posy + 1][posx] < dist[posy][posx] && !hwalls[posx][posy + 1]) {
-                cell.res = true;
-                cell.x = posx;
-                cell.y = posy;
-                return cell;
+        if(cell.y < size - 1) { //S
+            if(!hwalls[posx][posy + 1] && dist[cell.y + 1][cell.x] < leastDist) {
+                retCell.x = cell.x;
+                retCell.y = cell.y + 1;
+                leastDist = dist[cell.y + 1][cell.x];
             }
         }
-        if(posy > 0) { //N
-            if(dist[posy - 1][posx] < dist[posy][posx] && !hwalls[posx][posy]) {
-                cell.res = true;
-                cell.x = posx;
-                cell.y = posy;
-                return cell;
+        if(cell.y > 0) { //N
+            if(!hwalls[posx][posy] && dist[cell.y - 1][cell.x] < leastDist) {
+                retCell.x = cell.x;
+                retCell.y = cell.y - 1;
+                leastDist = dist[cell.y - 1][cell.x];
             }
         }
-        return cell;
+        return retCell;
     }
 
     public static void turnRight() {
@@ -219,7 +260,8 @@ public class Micromouse {
         printMatrix();
         while(!goal) {
             System.out.println("At " + posx + "," + posy);
-            if(LessAvailableNeighbour().res){
+            System.out.println("dir: " + dir);
+            if(lesserAvailableNeighbourCell(new Cell(posx, posy)).res){
                 System.out.println("in LessAvailableNeighbour().res true");
                 if(!checkFrontWall()) {
                     System.out.println("going front");
@@ -235,14 +277,16 @@ public class Micromouse {
                 }
             } else {
                 System.out.println("Renumbering...");
-                //renumber
                 Queue<Cell> queue = new ArrayDeque<>();
                 queue.add(new Cell(posx, posy));
                 while(!queue.isEmpty()) {
                     Cell currCell = queue.remove();
-                    Cell lessCell = LessAvailableNeighbour();
-                    if(lessCell.res) {
-                        dist[currCell.y][currCell.x] = dist[lessCell.y][lessCell.x] + 1;
+                    System.out.println("currCell: "+currCell.x+","+currCell.y);
+                    Cell lessCell = lesserAvailableNeighbourCell(currCell);
+                    if(!lessCell.res) {
+                        Cell leastCell = leastAvailableNeighbourCell(currCell);
+                        System.out.println("leastCell: "+leastCell.x+","+leastCell.y);
+                        dist[currCell.y][currCell.x] = dist[leastCell.y][leastCell.x] + 1;
                         if(!checkNorthWall(currCell)) {
                             queue.add(new Cell(posx, posy - 1));
                         } else if(!checkEastWall(currCell)) {
@@ -256,6 +300,9 @@ public class Micromouse {
                 }
                 printMatrix();
                 Thread.sleep(1000);
+            }
+            if(posx == targetx && posy == targety) {
+                goal = true;
             }
         }
     }
