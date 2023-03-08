e886936ad5e85fb3e7e05f1acf4496adc913f972
Prajwa-l-Kumar <prajwalkumar.bt22cse@pec.edu.in>
2023-03-08 18:39:41 +0530

    Updated Micromouse.java

--- main/Micromouse.java
+++ main/Micromouse.java
@@ -1,4 +1,8 @@
 package main;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 /**
  * Micromouse
  */
@@ -12,14 +16,16 @@ public class Micromouse {
     static int dist[][] = new int[size][size];
     static boolean visited[][] = new boolean[size][size];
     static int posx, posy;
+    static char dirs[] = {'N', 'E', 'S', 'W'};
     static char dir;
+    static boolean goal;
 
     public static void setup() {
         for(int i = 0; i < hwallsVal.length; i++) {
             hwalls[hwallsVal[i][0]][hwallsVal[i][1]] = true;
         }
-        for(int i = 0; i < hwallsVal.length; i++) {
-            hwalls[vwallsVal[i][0]][vwallsVal[i][1]] = true;
+        for(int i = 0; i < vwallsVal.length; i++) {
+            vwalls[vwallsVal[i][0]][vwallsVal[i][1]] = true;
         }
         if(size % 2 == 0) {
             for(int i = 0; i < size/2; i++) {
@@ -35,6 +41,16 @@ public class Micromouse {
             }
         }
 
+        dir = 'N';
+
+        posx = size - 1;
+        posy = 0;
+
+        goal = false;
+    }
+
+    public static void printDist() {
+
         for(int i = 0; i < dist.length; i++) {
             for(int j = 0; j < dist[i].length; j++) {
                 System.out.print(dist[i][j] + " ");
@@ -42,10 +58,44 @@ public class Micromouse {
             System.out.println();
         }
 
-        dir = 'N';
     }
 
-    static boolean checkLeft() {
+    public static void printMatrix() {
+        for(int i = 0; i < size; i++) {
+            System.out.print("+ ");
+            for(int  j = 0; j < size; j++) {
+                System.out.print((hwalls[i][j] ? "--" : "  ") + "+ ");
+            }
+            System.out.println();
+            for(int  j = 0; j < size; j++) {
+                System.out.print((vwalls[i][j] ? "| " : "  ") + "" + dist[i][j]+" ");
+            }
+            System.out.println("|");
+        }
+        System.out.print("+ ");
+        for (int i = 0; i < size; i++) {
+            System.out.print("--+ ");
+        }
+        System.out.println();
+    }
+
+    static boolean checkNorthWall(Cell cell) {
+        return hwalls[cell.x][cell.y];
+    }
+
+    static boolean checkEastWall(Cell cell) {
+        return cell.x == size - 1 ? true : vwalls[cell.x + 1][cell.y];
+    }
+
+    static boolean checkSouthWall(Cell cell) {
+        return cell.y == size - 1 ? true : hwalls[cell.x][cell.y + 1];
+    }
+
+    static boolean checkWestWall(Cell cell) {
+        return vwalls[cell.x][cell.y];
+    }
+
+    static boolean checkLeftWall() {
         switch(dir) {
             case 'N':
                 return vwalls[posx][posy];
@@ -60,7 +110,7 @@ public class Micromouse {
         }
     }
 
-    static boolean checkRight() {
+    static boolean checkRightWall() {
         switch(dir) {
             case 'N':
                 return posx == size - 1 ? true : hwalls[posx + 1][posy];
@@ -74,9 +124,127 @@ public class Micromouse {
                 return false;
         }
     }
+
+    static boolean checkFrontWall() {
+        switch(dir) {
+            case 'N':
+                return hwalls[posx][posy];
+            case 'E':
+                return posx == size - 1 ? true : vwalls[posx + 1][posy];
+            case 'S':
+                return posy == size - 1 ? true : hwalls[posx][posy + 1];
+            case 'W':
+                return vwalls[posx][posy];
+            default :
+                return false;
+        }
+    }
+
+
+    public static Cell LessAvailableNeighbour() {
+        Cell cell = new Cell(-1, -1, false);
+        if(posx < size - 1) {
+            if(dist[posx + 1][posy] < dist[posx][posy] && vwalls[posx + 1][posy]) {
+                cell.res = true;
+                cell.x = posx;
+                cell.y = posy;
+            }
+        } else if(posx > 0) {
+            if(dist[posx - 1][posy] < dist[posx][posy] && hwalls[posx - 1][posy]) {
+                cell.res = true;
+                cell.x = posx;
+                cell.y = posy;
+            }
+        } else if(posy < size - 1) {
+            if(dist[posx][posy + 1] < dist[posx][posy] && vwalls[posx][posy + 1]) {
+                cell.res = true;
+                cell.x = posx;
+                cell.y = posy;
+            }
+        } else if(posy > 0) {
+            if(dist[posx][posy - 1] < dist[posx][posy] && vwalls[posx][posy - 1]) {
+                cell.res = true;
+                cell.x = posx;
+                cell.y = posy;
+            }
+        }
+        return cell;
+    }
+
+    public static void turnRight() {
+        for(int i = 0; i < dirs.length; i++) {
+            if(dirs[i] == dir) {
+                dir = dirs[(i + 1) % dirs.length];
+                break;
+            }
+        }
+    }
+
+    public static void turnLeft() {
+        for(int i = 0; i < dirs.length; i++) {
+            if(dirs[i] == dir) {
+                dir = dirs[(i + dirs.length - 1) % dirs.length];
+                break;
+            }
+        }
+    }
+
+    public static void goAhead(int i) {
+        switch(dir) {
+            case 'N':
+                posy -= i;
+                break;
+            case 'E':
+                posx += i;
+                break;
+            case 'S':
+                posy += i;
+                break;
+            case 'W':
+                posx -= i;
+                break;
+        }
+    }
     
     public static void main(String[] args) {
         setup();
+        printMatrix();
+        while(!goal) {
+            System.out.println("At " + posx + "," + posy);
+            if(LessAvailableNeighbour().res){
+                if(!checkFrontWall()) {
+                    goAhead(1);
+                } else if(!checkRightWall()) {
+                    turnRight();
+                    goAhead(1);
+                } else if(!checkLeftWall()) {
+                    turnLeft();
+                    goAhead(1);
+                }
+            } else {
+                System.out.println("Renumbering");
+                //renumber
+                Queue<Cell> queue = new ArrayDeque<>();
+                queue.add(new Cell(posx, posy));
+                while(!queue.isEmpty()) {
+                    Cell currCell = queue.remove();
+                    Cell lessCell = LessAvailableNeighbour();
+                    if(!lessCell.res) {
+                        dist[currCell.x][currCell.y] = dist[lessCell.x][lessCell.y] + 1;
+                        if(!checkNorthWall(currCell)) {
+                            queue.add(new Cell(posx, posy - 1));
+                        } else if(!checkEastWall(currCell)) {
+                            queue.add(new Cell(posx + 1, posy));
+                        } else if(!checkSouthWall(currCell)) {
+                            queue.add(new Cell(posx, posy + 1));
+                        } else if(!checkWestWall(currCell)) {
+                            queue.add(new Cell(posx - 1, posy));
+                        }
+                    }
+                }
+                printMatrix();
+            }
+        }
     }
 
 }
